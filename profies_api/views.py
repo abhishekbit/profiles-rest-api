a8adb69b60fa4cac8b2043207fada744b7c0997b
Abhishek <abhishek1111993@gmail.com>
2022-05-10 22:44:01 +0530

    added api view

--- profies_api/views.py
+++ profies_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from sys import api_version
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    def get(self, request, format=None):
+        api_view=[
+            'uses http method (get,post,put,delete) ',
+            'Is similar to a django view',
+            'gives you most control over application logic',
+            'mapped manually to URLs'
+        ]
+        return Response({'messsage':'Hello', 'api_view':api_view })
